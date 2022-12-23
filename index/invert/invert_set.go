38f28499ba6ab1d5a2138f510199added962b6b3
Ivanqi <512350225@qq.com>
2022-12-23 12:14:02 +0800

    【开发】增加倒排索引

---
+++ index/invert/invert_set.go
@@ -0,0 +1,237 @@
+package invert
+
+import (
+	"./tools"
+	"./store"
+	"./mlog"
+	"fmt"
+	"./message"
+	"strings"
+	"encoding/json"
+	"encoding/binary"
+)
+
+// 倒排索引集合
+type InvertSet struct {
+	name string
+	path string
+	mode tools.FalconMode
+	fieldInformations map[string]*tools.FalconFieldInfo
+
+	// 字符串倒排写服务
+	stringInvertWriteServices map[string]FalconStringInvertWriteService
+	// 字符串倒排读服务
+	stringInvertReadServices map[string]FalconStringInvertReadService
+
+	invertListStoreReader store.FalconSearchStoreReadService
+	dictStoreReader store.FalconSearchStoreReadService
+}
+
+func NewInvertSet(name string, path string) FalconInvertSetService {
+	is := &InvertSet{
+		name:name, path: path,
+		fieldInformations:make(map[string]*tools.FalconFieldInfo),
+		stringInvertReadServices:make(map[string]FalconStringInvertReadService),
+		stringInvertWriteServices:make(map[string]FalconStringInvertWriteService)
+	}
+
+	metaFile := path + "/" + name + ".mt"
+	ivtFile := path + "/" + name + ".ivt"
+	dicFile := path + "/" + name + ".dic"
+
+	if tools.Exists(metaFile) && tools.Exists(ivtFile) && tools.Exists(dicFile) {
+		is.invertListStoreReader = store.NewFalconSearchStoreReadService(ivtFile)
+		is.dictStoreReader = store.NewFalconSearchStoreReadService(dicFile)
+
+		if err := is.init(); err != nil {
+			return nil
+		}
+
+		mlog.Trace("Load InvertSetService with read mode success ...")
+		return is
+	}
+
+	is.mode = tools.TWriteMode
+
+	mlog.Trace("Start InvertSetService with write mode success ...")
+
+	return is
+}
+
+func (is *InvertSet) init() error {
+	if err := is.loadMeta(); err != nil {
+		mlog.Error("load meta error: %v", err)
+		return err
+	}
+
+	is.mode = tools.TReadMode
+	return nil
+}
+
+func (is *InvertSet) AddField(field string, fieldType tools.FalconFieldType) error {
+	if _, ok = is.fieldInformations[field]; ok {
+		return fmt.Errorf("Field [ %s ] is already exist", field)
+	}
+
+	is.fieldInformations[field] = &tools.FalconFieldInfo{Name:field, Type:fieldType, Offset:0}
+
+	switch fieldType{
+	case tools.TFalconString:
+		is.stringInvertWriteServices[field] = NewStringInvertReader(is.name)
+	default:
+		mlog.Error("unkown field type %d", fieldType)
+		return fmt.Errorf("unkown field type")
+	}
+
+	return nil
+}
+
+func (is *InvertSet) PutString(field, key string, docid *message.DocId) error {
+	if is.mode & tools.TWriteMode != tools.TWriteMode {
+		mlog.Error("not write mode ...")
+		return fmt.Errorf("not write mode")
+	}
+
+	if ivtWriter, ok := is.stringInvertWriteServices[field]; !ok {
+		return fmt.Errorf("Field is not string mode or not found ...")
+	} else {
+		return ivtWriter.Put(key.docid)
+	}
+}
+
+func (is *InvertSet) FetchString(field, key string) (FalconDocList, bool, error) {
+	if is.mode & tools.TReadMode != tools.TReadMode {
+		mlog.Error("not write mode...")
+		return nil, false, fmt.Errorf("not write mode")
+	}
+
+	if ivtReader, ok := is.stringInvertReadServices[field]; !ok {
+		return nil, false, fmt.Errorf("Faild is not string mode or not found ...")
+	} else {
+		return ivtReader.Fetch(key)
+	}
+}
+
+func (is *InvertSet) Persistence() error {
+	// 模式判断
+	if is.mode & tools.TWriteMode != tools.TWriteMode {
+		mlog.Error("not write mode ...")
+		return fmt.Errorf("not write mode")
+	}
+
+	ivtFile := is.path + "/" + is.name + ".ivt"
+	dicFile := is.path + "/" + is.name + ".dic"
+
+	invertListStoreWriter := store.NewFalconFileStoreWriteService(ivtFile)
+	dictStoreWriter := store.NewFalconFileStoreWriteService(dicFile)
+
+	// 持久化数据
+	for fieldName, ivtWriter := range is.stringInvertWriteServices {
+		offset, err := ivtWriter.Persistence(invertListStoreWriter, dictStoreWriter)
+		if err != nil {
+			mlog.Error("Persistence [ %s ] failure : %v", fieldName, err)
+			return err
+		}
+
+		is.fieldInformations[fieldName].Offset = offset
+	}
+
+	invertListStoreWriter.Close()
+	dictStoreWriter.Close()
+
+	// 持久化元数据
+	is.storeMeta()
+
+	// 重新以只读方式读取所有数据
+	is.invertListStoreReader = store.NewFalconSearchStoreReadService(is.path + "/" + is.name + ".ivt")
+	is.dictStoreReader = store.NewFalconSearchStoreReadService(is.path + "/" + is.name + ".dic")
+
+	for _, fi := range is.fieldInformations {
+		is.stringInvertReadServices[fi.Name] = NewStringInvertReader(fi.Name, fi.Offset, is.dictStoreReader, is.invertListStoreReader)
+	}
+
+	is.mode = tools.TReadMode
+
+	return nil
+}
+
+func (is *InvertSet) Close() error {
+	is.dictStoreReader.Close()
+	is.invertListStoreReader.Close()
+
+	return nil
+}
+
+func (is *InvertSet) ToString() string {
+	info := make([]string, 0)
+	for _, fi := range is.fieldInformations {
+		info = append(info, fi.ToString)
+	}
+
+	return fmt.Sprintf("[[\n%s\n]]", strings.Join(info, "\n"))
+}
+
+func (is *InvertSet) FalconEncoding() ([]byte, error) {
+	bj, err := json.Marshal(is.fieldInformations)
+	if err != nil {
+		mlog.Error("json: %v", err)
+		return nil, err
+	}
+
+	return bj, nil
+}
+
+func (is *InvertSet) FalconDecoding(bytes []byte) error {
+	json.Unmarshal(bytes, &is.fieldInformations)
+
+	for _, fi := range is.fieldInformations {
+		is.stringInvertReadServices[fi.Name] = NewStringInvertReader(fi.Name, fi.Offset, is.dictStoreReader, is.invertListStoreReader)
+	}
+
+	return nil
+}
+
+func (is *InvertSet) loadMeta() error {
+	metaFile := is.path + "/" + is.name + ".mt"
+
+	metaReader := store.NewFalconSearchStoreReadService(metaFile)
+
+	lensBytes, err := metaReader.ReadFullBytes(0, 8)
+	if err != nil {
+		return err
+	}
+
+	metaBytes, err := metaReader.ReadFullBytes(8, int64(binary.LittleEndian.Uint64(lensBytes)))
+	if err != nil {
+		mlog.Error("read meta bytes error : %v", err)
+		return err
+	}
+
+	if err := is.FalconDecoding(metaBytes); err != nil {
+		mlog.Error("decoding error : %v", err)
+		return err
+	}
+
+	return metaReader.Close()
+}
+
+func (is *InvertSet) storeMeta() error {
+	metaFile := is.path + "/" + is.name + ".mt"
+
+	// 持久化元数据
+	metaStoreWriter := store.NewFalconFileStoreWriteService(metaFile)
+	metaBytes, err := is.FalconEncoding()
+	if err != nil {
+		mlog.Error(" encoding meta error : %v", err)
+		return err
+	}
+
+	// 写入长度
+	metaLensBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(metaLensBytes,uint64(len(metaBytes)))
+	metaStoreWriter.AppendBytes(metaLensBytes)
+	metaStoreWriter.AppendBytes(metaBytes)
+	metaStoreWriter.Close()
+
+	return nil
+}
---
+++ index/invert/invert_test.go
@@ -0,0 +1,148 @@
+package invert
+
+import (
+	"testing"
+	"./mlog"
+	"fmt"
+	"./store"
+	"./message"
+	"./tools"
+)
+
+func Test_InvertWriter(t *testing.T) {
+	mlog.Start(mlog.LevelInfo, "iw.log")
+
+	invertListStore := store.NewFalconFileStoreWriteService("./abc.ivt")
+	dictStore := store.NewFalconFileStoreWriteService("./abc.dic")
+
+	iw := NewStringInvertWriter("abc")
+
+	iw.Put("abc",&message.DocId{DocID:0,Weight:10})
+	iw.Put("abc",&message.DocId{DocID:2,Weight:12})
+	iw.Put("abc",&message.DocId{DocID:3,Weight:13})
+	iw.Put("a",&message.DocId{DocID:2,Weight:14})
+	iw.Put("a",&message.DocId{DocID:9,Weight:19})
+
+	iw.Put("b",&message.DocId{DocID:4,Weight:14})
+	iw.Put("b",&message.DocId{DocID:9,Weight:19})
+
+	iw.Persistence(invertListStore,dictStore)
+	invertListStore.Close()
+	dictStore.Close()
+	mlog.Info(" %s ",iw.ToString())
+}
+
+func Test_InvertReader(t *testing.T) {
+	invertListStore := store.NewFalconSearchStoreReadService("./abc.ivt")
+	dictStore := store.NewFalconSearchStoreReadService("./abc.dic")
+
+	ir := NewStringInvertReader("abc",0,dictStore,invertListStore)
+	fetch(ir,"a")
+	fetch(ir,"abc")
+	fetch(ir,"c")
+	invertListStore.Close()
+	dictStore.Close()
+}
+
+func Test_InvertInsert(t *testing.T) {
+
+	invertListStore := store.NewFalconFileStoreWriteService("./ivt_insert.ivt")
+	dictStore := store.NewFalconFileStoreWriteService("./ivt_insert.dic")
+
+	iw := NewStringInvertWriter("ivt_insert")
+
+	for i:=uint32(0);i<1000;i++{
+		iw.Put(fmt.Sprintf("k%d",i),&message.DocId{DocID:i,Weight:i+10})
+	}
+	iw.Persistence(invertListStore,dictStore)
+	invertListStore.Close()
+	dictStore.Close()
+	//mlog.Info("%s",iw.ToString())
+
+}
+
+func Test_InvertReader2(t *testing.T) {
+
+	invertListStore := store.NewFalconSearchStoreReadService("./ivt_insert.ivt")
+	dictStore := store.NewFalconSearchStoreReadService("./ivt_insert.dic")
+
+	ir := NewStringInvertReader("ivt_insert",0,dictStore,invertListStore)
+	fetch(ir,"a")
+	fetch(ir,"abc")
+	fetch(ir,"k50")
+	invertListStore.Close()
+	dictStore.Close()
+
+}
+
+func fetch(ir FalconStringInvertReadService,key string) {
+	doclist,found,err:=ir.Fetch(key)
+	if err!=nil{
+		mlog.Error("Key [ %s ] fatch error : %v",key ,err)
+		return
+	}
+	if found {
+		mlog.Info("Key [ %s ] >>> %s",key,doclist.ToString())
+		return
+	}
+	mlog.Warning("Key [ %s ] not found",key)
+}
+
+func Test_InvertSetTest(t *testing.T) {
+
+	invertSetService := NewInvertSet("segment",".")
+	invertSetService.AddField("testfield1",tools.TFalconString)
+	invertSetService.AddField("testfield2",tools.TFalconString)
+
+	for i:=uint32(0);i<1000;i++{
+		invertSetService.PutString("testfield1",fmt.Sprintf("k%d",i),&message.DocId{DocID:i,Weight:i+10})
+		invertSetService.PutString("testfield2",fmt.Sprintf("key%d",i),&message.DocId{DocID:i,Weight:i+10})
+
+	}
+
+	invertSetService.Persistence()
+
+	doclist,found,err:=invertSetService.FetchString("testfield2","key88")
+
+	if err!=nil{
+		mlog.Error("Test_InvertSetTest Key [ k88 ] fatch error : %v" ,err)
+		return
+	}
+	if found {
+		mlog.Info("Test_InvertSetTest Key [ k88 ] >>> %s",doclist.ToString())
+		return
+	}
+	mlog.Warning("Test_InvertSetTest Key [ k88 ] not found")
+
+
+}
+
+func fetchField(field,key string,invertSetService FalconInvertSetService) error {
+	doclist,found,err:=invertSetService.FetchString(field,key)
+
+	if err!=nil{
+		mlog.Error("Test_InvertSetTest Field [ %s ] Key [ %s ] fatch error : %v" ,field,key,err)
+		return err
+	}
+	if found {
+		mlog.Info("Test_InvertSetTest Field [ %s ] Key [ %s ] >>> %s",field,key,doclist.ToString())
+		return nil
+	}
+	mlog.Warning("Test_InvertSetTest Field [ %s ] Key [ %s ] not found",field,key)
+	return nil
+}
+
+func Test_InvertSetReadTest(t *testing.T) {
+
+	invertSetService := NewInvertSet("segment",".")
+
+	fetchField("testfield1","k876",invertSetService)
+	fetchField("testfield2","key876",invertSetService)
+	fetchField("testfield1","fdet",invertSetService)
+	fetchField("fieldddd1","k876",invertSetService)
+
+	invertSetService.Close()
+
+	mlog.Info("set info : %s ",invertSetService.ToString())
+
+}
